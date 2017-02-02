10e53bf5f6d288b968e7a23b294d19d5bf430a38
Mikhail Delport <mikhail@delport.ca>
2017-02-01 20:16:25 -0500

    Reference link for API nested resource creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
     has_many :options, inverse_of: :event
 
     # Allows for creation of Options at the same time as the event
+    # https://forum.upcase.com/t/api-design-routing-with-nested-resources/6570
     accepts_nested_attributes_for :options
 end
