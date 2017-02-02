38e5e246fc7bd802d9dbbb34d46ff21463609c22
Mikhail Delport <mikhail@delport.ca>
2017-02-02 17:54:10 -0500

    Remove option controller and unused methods in event controller
    Also cleanup the routes available

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :options
-  resources :events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events, only: [:index, :show, :create]
 end
