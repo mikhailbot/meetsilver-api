b84cef415138bdc270113521f19f44147af6bece
mikhaildelport <mikhail@delport.ca>
2017-04-22 13:52:04 -0400

    Add Ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20170208230347) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "slug"
-    t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "location"
   end
 
   create_table "options", force: :cascade do |t|
